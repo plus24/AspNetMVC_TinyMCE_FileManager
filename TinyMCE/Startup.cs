1383a309078fefb2b0c7d75dce7bd80945bfc9f9
Masoud ehsani <meplus@live.com>
2015-12-06 17:14:07 +0330

    Begin

---
+++ TinyMCE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TinyMCE.Startup))]
+namespace TinyMCE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
